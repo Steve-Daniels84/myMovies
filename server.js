7fc90475f1d13ddbc794b9437e3066fd03484cb4
Steve D <111876089+Stevil-Kanevil@users.noreply.github.com>
2024-05-07 13:04:54 +0100

    project setup

--- server.js
+++ server.js
@@ -1,36 +1,43 @@
+//import http, file system and url modules
 const http = require('http'),
-  fs = require('fs'),
-  url = require('url');
+url = require('url'),
+fs = require('fs');
 
+//create server
 http.createServer((request, response) => {
-  let addr = request.url,
-    q = new URL(addr, 'http://' + request.headers.host),
-    filePath = '';
+    
+    let addr = request.url, //set variable for requested url
+    q = new URL(addr, 'http://' + request.headers.host), //create new URL
+    filePath = ''; //set filepath varible for redirect
 
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-    if (err) {
-      console.log(err);
+    //implement logging on request
+    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Log entry added!')
+        }
+    })
+
+    //requested page redirect
+    if (q.pathname.includes('documentation')) {
+        filePath = __dirname + '/documentation.html' //if documentation is requested then route to documentation html
     } else {
-      console.log('Added to log.');
-    }
-  });
-
-  if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
-  } else {
-    filePath = 'index.html';
-  }
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      throw err;
+        filePath = __dirname + '/index.html' //open index.html if requested file does nto exist
     }
 
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
-    response.end();
+    fs.readFile (filePath, (err, data) => {
+        if (err) {
+            throw err;
+        }
 
-  });
+        response.writeHead (200, {'Content-Type': + 'text/html'});
+        response.write (data);
+        response.end()
+    })
+
+
 
 }).listen(8080);
-console.log('My test server is running on Port 8080.');
+console.log('Sever running!');
