f03c5ebe5df6d5b3d1722bb48da016e2f206fa61
Steve D <111876089+Stevil-Kanevil@users.noreply.github.com>
2024-05-08 22:32:15 +0100

    adding movie data

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-const express = require('express'),
+//access modules
+const express = require('express'), 
     morgan = require('morgan'),
     fs = require('fs'),
     path = require('path');
 
 app = express();
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+//create log stream
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'}); 
 
+//movie data
 const faveMovies = {
     "films":[
-        {"title":"1"},
-        {"title":"2"}
+        {"title":"Dune","year":"2022","genre":"Sci-fi"},
+        {"title":"Predator","year":"1988","genre":"Action"},
+        {"title":"The Notebook","year":"2003","genre":"Romance"},
+        {"title":"Moneyball","year":"2022","genre":"Sport History"},
+        {"title":"The Terminator","year":"1986","genre":"Sci-fi"},
+        {"title":"Mortal Engines","year":"2012","genre":"Fantasy"}
     ]    
 }
 
-app.use(morgan('combined', {stream: accessLogStream}));
+//logstream middleware
+app.use(morgan('combined', {stream: accessLogStream})); 
 
 app.use(express.static('public'));
 
+//error handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Something broke!');
+    res.status(500).send('Internal server error');
   });
 
 app.get('/', (req, res) => {
