37eed96971cae225c38c800deb31c7ed5010c091
Steve D <111876089+Stevil-Kanevil@users.noreply.github.com>
2024-05-22 14:29:21 +0100

    refactoring for database

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express'),
     fs = require('fs'),
     path = require('path'),
     bodyParser = require('body-parser'),
-    uuid = require('uuid'),
     mongoose = require('mongoose'),
     models = require('./models.js'),
     movies = models.Movie,
@@ -19,106 +18,6 @@ app.use(bodyParser.json());
 
 //create log stream
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'}); 
-
-// let faveMovies = 
-//     [
-//   {
-//     '_id': '664a60e30f0177a700e79dc0',
-//     'Title': 'The Avengers',
-//     'Description': "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity.",
-//     'Genre': {
-//       'Name': 'Action',
-//       'Description': 'Action film is a genre in which the protagonist or protagonists are thrust into a series of events that typically include violence, extended fighting, physical feats, and frantic chases.'
-//     },
-//     'Director': {
-//       'Name': 'Joss Whedon',
-//       'Bio': 'Joseph Hill Whedon is an American film director, producer, and screenwriter.',
-//       'Birth': '1964'
-//     },
-//     'ImagePath': 'avengers.png',
-//     'Featured': true,
-//     'movieId': '00001'
-//   },
-//   {
-//     '_id': '664a615e0f0177a700e79dc1',
-//     'Title': 'Titanic',
-//     'Description': 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.',
-//     'Genre': {
-//       'Name': 'Romance',
-//       'Description': 'Romance film is a genre that focuses on the romantic relationships between characters.'
-//     },
-//     'Director': {
-//       'Name': 'James Cameron',
-//       'Bio': 'James Francis Cameron is a Canadian filmmaker and environmentalist.',
-//       'Birth': '1954'
-//     },
-//     'ImagePath': 'titanic.png',
-//     'Featured': true,
-//     'movieId': '00002'
-//   },
-//   {
-//     '_id': '664a6b0e0f0177a700e79dc7',
-//     'Title': 'The Lion King',
-//     'Description': 'Lion prince Simba and his father are targeted by his bitter uncle, who wants to ascend the throne himself.',
-//     'Genre': {
-//       'Name': 'Animation',
-//       'Description': 'Animation film is a genre in which the images are primarily created through animation techniques.'
-//     },
-//     'Director': {
-//       'Name': 'Roger Allers and Rob Minkoff',
-//       'Bio': 'Roger Allers is an American film director, screenwriter, and animator. Rob Minkoff is an American filmmaker.',
-//       'Birth': '1949 (Allers), 1962 (Minkoff)'
-//     },
-//     'ImagePath': 'lionking.png',
-//     'Featured': true,
-//     'movieId': '00010'
-//   }
-// ];
-
-// const users = [
-    
-//     {
-//       'id': '664a646d0f0177a700e79dc2',
-//       'Username': 'Stevil Kanevil',
-//       'Email': 'test1@test.com',
-//       'password': '123456',
-//       'Birthdate': '1984-02-02T00:00:00.000Z',
-//       'favouriteMovies': [ '00008', '00010']
-//     },
-//     {
-//       'id': '664a66110f0177a700e79dc3',
-//       'Username': 'test1',
-//       'Email': 'test2@test.com',
-//       'password': '123456',
-//       'Birthdate': '1999-01-09T00:00:00.000Z',
-//       'favouriteMovies': [ '00002', '00010', '00003', '00007', '00002' ]
-//     },
-//     {
-//       'id': '664a66110f0177a700e79dc4',
-//       'Username': 'test2',
-//       'Email': 'test3@test.com',
-//       'password': '1234556',
-//       'Birthdate': '1994-04-06T00:00:00.000Z',
-//       'favouriteMovies': [ '00010', '00005', '00004' ]
-//     },
-//     {
-//       'id': '664a66110f0177a700e79dc5',
-//       'Username': 'test3',
-//       'Email': 'test4@test.com',
-//       'password': '123456',
-//       'Birthdate': '2001-04-06T00:00:00.000Z',
-//       'favouriteMovies': [ '00007', '00001', '00002' ]
-//     },
-//     {
-//       'id': '664a66e10f0177a700e79dc6',
-//       'Username': 'test4',
-//       'Email': 'test4@test.com',
-//       'password': '123456',
-//       'Birthdate': '2012-04-05T00:00:00.000Z',
-//       'favouriteMovies': [ '00005', '00010', '00006' ]
-//     }
-//   ]
-
 
 //logstream middleware
 app.use(morgan('combined', {stream: accessLogStream})); 
@@ -136,19 +35,42 @@ app.get('/', (req, res) => {
     res.status(200).sendFile('documentation.html', {root: __dirname + '/public'});
 })
 
-//Add new movie into dataset
-app.post('/movies', (req, res) => {
-    const message = 'No movie data in request body'
-    let newMovie = {};  
-
-    if (!req.body) {
-        res.status(400).send(message)
-    } else {
-        newMovie = req.body;
-        newMovie.id = uuid.v4();
-        faveMovies.push(newMovie);
-        res.status(201).send(newMovie);
-    }
+Add new movie into dataset
+app.post('/movies', async (req, res) => {
+    await movies.findOne({Title: req.body.Title})
+        .then((movie) => {
+            if (movie) {
+                return res.status(400).send('Movie already exists')
+            } else {
+                console.log(req.body);
+                movies
+                    .create({
+                            Genre: {
+                              Name: req.body.Genre.Name,
+                              Description: req.body.Genre.Description,
+                            },
+                            Director: {
+                              Name: req.body.Director.Name,
+                              Bio: req.body.Director.Bio,
+                              Birth: req.body.Director.Birth
+                            },
+                            Actors: req.body.Actors,
+                            Title: req.body.Title,
+                            Description: req.body.Description,
+                            ImagePath: req.body.ImagePath,
+                            Featured: req.body.Featured,
+                    })
+                .then((movie) => {
+                    res.status(201).json(movie)
+                })
+                .catch((error) => {
+                    res.status(500).send('Error: ' + error);
+                })
+            }
+        })
+        .catch((error) => {
+            res.status(500).send('Error: ' + error);
+        })
 })
 
 //Get all movies
@@ -158,7 +80,7 @@ app.get('/movies', (req, res) => {
 
 //find movie by title
 app.get('/movies/:Title', (req, res) => {
-movies.find({'Title': req.params.Title})
+movies.find({Title: req.params.Title})
 .then((movies)=> {
         res.status(200).json(movies);
     }).catch((err) => {
@@ -295,12 +217,24 @@ app.post('/users/:id/:movieId', async (req,res) => {
 })
 
 //Deletes a favourite movie from a user
-app.delete('/users/:id/:movieId', (req,res) => {
-    const user = users.find(user => {return user.id === req.params.id});
-    const userMovies = user.favouriteMovies;
-    const index = userMovies.indexOf(req.params.movieId);
-    userMovies.splice(index, index);
-    res.send('test');
+app.delete('/users/:id/:movieId', async (req,res) => {
+    try {
+        const userId = req.params.id;
+        const movieId = req.params.movieId;
+
+        const updatedUser = await users.findOneAndUpdate(
+            {"_id" : req.params.id},  
+            {$pull: {FavouriteMovies: req.params.movieId}},
+            {new:true}
+        );
+
+        if (!updatedUser) {
+            return res.status(404).send({message: 'User not found'});        
+        }
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        res.status(500).send({message: 'Error: ' + error});
+    }
 })
 
 app.listen(8080, () => {
