e15f9aad490f5761c2e4091ff683e6a4bb21f746
Steve D <111876089+Stevil-Kanevil@users.noreply.github.com>
2024-06-03 21:59:12 +0100

    adding auth and jwt

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ async function listUsers (req, res) {
    .catch(err => {
     res.status(400).send('Error: ' + err);
    })
-}
+};
 
 async function getUserById (req, res) {
     await Users.find({'_id': req.params.id})
@@ -24,7 +24,7 @@ async function getUserById (req, res) {
     .catch(err => {
         res.status(400).send('Error: ' + err);
     })
-}
+};
 
 async function addUser (req, res) {
     await Users.findOne({Username: req.body.Username})
@@ -49,8 +49,58 @@ async function addUser (req, res) {
         console.error(error);
         res.status(500).send('Error: ' + error);
     })
+};
+
+async function deleteUser (req, res) {
+    const userId = req.params.id;
+    await Users.findOneAndDelete({ _id: req.params.id})
+    .then((users) => {
+        if (!users) {
+            res.status(400).send('User ' + userId + ' was not found');
+            } else {
+            res.status(200).send('User ' + userId + ' was deleted');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        })
+};
+
+async function addMovie (req, res) {
+    await Users.findOneAndUpdate({ "_id":  req.params.id}, {
+        $push: {FavouriteMovies: req.params.movieId}
+        },
+        {new: true})
+    .then((updatedUser) => {
+        res.status(200).json(updatedUser);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    })
+}
+
+async function deleteMovie (req,res) {
+        try {
+        const userId = req.params.id;
+        const movieId = req.params.movieId;
+
+        const updatedUser = await Users.findOneAndUpdate(
+            {"_id" : req.params.id},  
+            {$pull: {FavouriteMovies: req.params.movieId}},
+            {new:true}
+        );
+
+        if (!updatedUser) {
+            return res.status(404).send({message: 'User not found'});        
+        }
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        res.status(500).send({message: 'Error: ' + error});
+    }
 }
 
 module.exports = {
-    listUsers, getUserById, addUser
+    listUsers, getUserById, addUser, deleteUser, addMovie, deleteMovie
 }
--- index.js
+++ index.js
@@ -47,81 +47,10 @@ app.delete('/movies/:id', movies.deleteMovieById); //Delete a movie by ID
 //User routes
 app.get('/users', users.listUsers); //Lists all users
 app.get('/users/:id', users.getUserById); //Gets a user by its id
-app.post('/users', users.addUser);
-
-// //Deletes a user from the dataset by its id
-// app.delete('/users/:id', async (req,res) => {
-//     const userId = req.params.id;
-//     await users.findOneAndDelete({ _id: req.params.id})
-//     .then((users) => {
-//         if (!users) {
-//             res.status(400).send('User ' + userId + ' was not found');
-//         } else {
-//             res.status(200).send('User ' + userId + ' was deleted');
-//         }
-//     })
-//     .catch((err) => {
-//         console.error(err);
-//         res.status(500).send('Error: ' + err);
-//     })
-// })
-
-// //Updates any of the values on the user record
-// app.put('/users/:id', async (req, res) => {
-//     await users.findOneAndUpdate({_id: req.params.id}, {
-//         $set:
-//             {
-//                 Username: req.body.Username,
-//                 Password: req.body.Password,
-//                 Email: req.body.Email
-//             }
-//         },
-//         {new:true})
-//     .then((updatedUser) => {
-//         res.status(201).json(updatedUser);
-//     })
-//     .catch((updatedUser) => {
-//         console.error(err);
-//         res.status(500).send('Error: ' + err);
-//     })
-// })
-
-
-// //Add a favourite movie to a user
-// app.post('/users/:id/:movieId', async (req,res) => {
-//     await users.findOneAndUpdate({ "_id":  req.params.id}, {
-//         $push: {FavouriteMovies: req.params.movieId}
-//     },
-//     {new: true})
-//     .then((updatedUser) => {
-//         res.status(200).json(updatedUser);
-//     })
-//     .catch((err) => {
-//         console.error(err);
-//         res.status(500).send('Error: ' + err);
-//     })
-// })
-
-// //Deletes a favourite movie from a user
-// app.delete('/users/:id/:movieId', async (req,res) => {
-//     try {
-//         const userId = req.params.id;
-//         const movieId = req.params.movieId;
-
-//         const updatedUser = await users.findOneAndUpdate(
-//             {"_id" : req.params.id},  
-//             {$pull: {FavouriteMovies: req.params.movieId}},
-//             {new:true}
-//         );
-
-//         if (!updatedUser) {
-//             return res.status(404).send({message: 'User not found'});        
-//         }
-//         res.status(200).json(updatedUser)
-//     } catch (error) {
-//         res.status(500).send({message: 'Error: ' + error});
-//     }
-// })
+app.post('/users', users.addUser); //Add a user
+app.delete('/users/:id', users.deleteUser); //Delete a user
+app.post('/users/:id/:movieId', users.addMovie); //Add a favourite movie to a user
+app.delete('/users/:id/:movieId', users.deleteMovie); //Deletes a fovurite movie from a user
 
 app.listen(8080, () => {
     console.log('Listening on port 8080');
